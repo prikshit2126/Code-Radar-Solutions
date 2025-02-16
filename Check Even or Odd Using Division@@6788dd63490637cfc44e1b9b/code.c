87ae653bc467a4facc8365edd5c3029b3d1cbd49
prikshit2126 <jaura2126@gmail.com>
2025-02-16 20:18:30 +0530

    Solution to Check Even or Odd Using Division

---
+++ Check Even or Odd Using Division@@6788dd63490637cfc44e1b9b/code.c
@@ -0,0 +1,11 @@
+// Your code here...
+#include <stdio.h>
+int main(){
+    int num, quotient;
+    scanf("%d %d",&num,&quotient);
+    if(num==(quotient*2)){
+        printf("Even");
+    }else{
+        printf("Odd");
+    }
+}
